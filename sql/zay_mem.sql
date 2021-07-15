4998a92ea2d89df985e01e620df0bc470fa591b6
yunji com <>
2021-07-15 10:00:25 +0900

    no message

--- sql/zay_mem.sql
+++ sql/zay_mem.sql
@@ -2,10 +2,10 @@
 -- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- 생성 시간: 21-07-12 08:33
+-- Host: localhost
+-- 생성 시간: 21-07-14 10:37
 -- 서버 버전: 10.4.19-MariaDB
--- PHP 버전: 7.3.28
+-- PHP 버전: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,10 +30,10 @@ SET time_zone = "+00:00";
 CREATE TABLE `zay_mem` (
   `ZAY_mem_idx` int(11) NOT NULL COMMENT '고유번호',
   `ZAY_mem_id` varchar(30) CHARACTER SET utf8 NOT NULL COMMENT '회원 아이디',
+  `ZAY_mem_pass` varchar(30) CHARACTER SET utf8 NOT NULL COMMENT '회원 비밀번호',
+  `ZAY_mem_pf` varchar(100) CHARACTER SET utf8 NOT NULL COMMENT '회원 프로필 사진',
   `ZAY_mem_name` varchar(30) CHARACTER SET utf8 NOT NULL COMMENT '회원 이름',
   `ZAY_mem_email` varchar(30) CHARACTER SET utf8 NOT NULL COMMENT '회원 이메일',
-  `ZAY_mem_pf` varchar(100) CHARACTER SET utf8 NOT NULL COMMENT '회원 프로필 사진',
-  `ZAY_mem_pass` varchar(30) CHARACTER SET utf8 NOT NULL COMMENT '회원 비밀번호',
   `ZAY_mem_regi_day` varchar(30) CHARACTER SET utf8 NOT NULL COMMENT '회원 가입일'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
